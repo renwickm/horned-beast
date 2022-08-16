0bd629e9b6a3d5a2eb5408edb0324aea543918d1
Matthew T. Renwick <mrenny65@gmail.com>
2022-08-15 22:38:39 -0700

    added footer and min to function on app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from './header.js';
 
+import Main from './Main.js';
+
+import Footer from './Footer.js';
+
 function App() {
   return (
     <div>
         <Header title="Code 301 Lab 1"/>
+        <Main/>
+        <Footer title="Matthew Renwick"/>
     </div>
   );
+
 }
 
 export default App;
