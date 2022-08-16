66ff6f1d481be3475c41237d7cad70a7f5e29b01
Matthew T. Renwick <mrenny65@gmail.com>
2022-08-15 22:37:49 -0700

    added footer and main file

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  // constructor(props){
+  //   super(props);
+  // }
+
+  render () {
+    return (
+      <Footer>
+        <h4>{this.props.title}</h4>
+      </Footer>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+  // constructor(props){
+  //   super(props);
+  // }
+
+  render () {
+    return (
+      <Main>
+
+      </Main>
+    )
+  }
+}
+
+export default Main;
